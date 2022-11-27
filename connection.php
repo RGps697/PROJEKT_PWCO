e196b095170919ee2b5ce6c86fead068fd56eb70
RG <radoslawg46@gmail.com>
2022-11-27 20:34:40 +0100

    connection added error

--- connection.php
+++ connection.php
@@ -1,11 +1,12 @@
 <?php
 $mysqli = new mysqli("todolistapp.cxlwfzkypbrr.us-east-1.rds.amazonaws.com", "root", "Pwco202223!App", "projektpwco");
 
-// if ($mysqli->connect_error) {
-//   die("Connection failed: " . $conn->connect_error);
-// }
-// else{
+if ($mysqli->connect_error) {
+  echo "Connection failed: " . $mysqli->connect_error;
+  die("Connection failed: " . $mysqli->connect_error);
+}
+else{
     echo "connected, hello world";
-// }
+}
 
 ?>
