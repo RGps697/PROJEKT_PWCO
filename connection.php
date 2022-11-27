b31ec1b21e372d16f644298ae5eacc022c919d3e
RG <radoslawg46@gmail.com>
2022-11-27 20:23:53 +0100

    aws database connection

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "projektpwco");
+$mysqli = new mysqli("todolistapp.cxlwfzkypbrr.us-east-1.rds.amazonaws.com", "root", "Pwco202223!App", "projektpwco");
 
 if ($mysqli->connect_error) {
   die("Connection failed: " . $conn->connect_error);
